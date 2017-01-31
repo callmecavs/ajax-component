6dee1c785a81d7047c62505cf38e67f620cc8d3b
callmecavs <callmecavs@gmail.com>
2017-01-31 18:35:49 -0500

    feature check

--- src/pjax-component.js
+++ src/pjax-component.js
@@ -1,5 +1,12 @@
 const component = () => {
+  // quick feature check
+  if (!'customElements' in window) {
+    // drop a warning in the console
+    console.warn('pjax-component: Custom Elements not supported in current browser.')
 
+    // exit early, can't create a Custom Element
+    return
+  }
 }
 
 export default component
