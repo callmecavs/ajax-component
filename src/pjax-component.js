4746a6d3bb9d99f68966de1d6b722dc0e6ff7955
callmecavs <callmecavs@gmail.com>
2017-02-01 19:47:51 -0500

    first take at some stuff

--- src/pjax-component.js
+++ src/pjax-component.js
@@ -1,4 +1,27 @@
-const component = () => {
+class PJAXComponent extends HTMLElement {
+  constructor () {
+    super()
+
+    // create a shadow root
+    this.shadow = this.attachShadow({ mode: 'open' })
+
+    // cache request urls
+    this.requests = [
+      'data-html',
+      'data-css',
+      'data-js'
+    ].reduce((accum, attr) => {
+      accum[attr] = this.getAttribute(attr)
+      return accum
+    }, {})
+
+    console.log('constructor')
+    console.log(this.shadow)
+    console.log(this.requests)
+  }
+}
+
+const component = name => {
   // quick feature check
   if (!'customElements' in window) {
     // drop a warning in the console
@@ -7,6 +30,10 @@ const component = () => {
     // exit early, can't create a Custom Element
     return
   }
+
+  // TODO: implement valid name check?
+  // attach the element using the name provided
+  window.customElements.define(name, PJAXComponent)
 }
 
 export default component
